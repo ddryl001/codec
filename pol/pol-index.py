d6a12e71beefa2f5ab7d5a47234f8ca8872296c3
ddryl001 <96310117+ddryl001@users.noreply.github.com>
2022-09-17 14:56:46 -0400

    Add files via upload
    The scripts have been changed to be more compatible with AntConc without the need for separate .txt and .tsv files. The data is still divided by tabs, so the text files can be changed to .tsv  easily. Pseudo xml tags are used to distinguish metadata from the main text.

---
+++ pol/pol-index.py
@@ -0,0 +1,30 @@
+import collections
+import csv
+import datetime
+import os
+import requests
+import time
+from bs4 import BeautifulSoup
+
+file = open("pol-index.csv", "w")
+writer = csv.writer(file)
+url = "https://boards.4chan.org/pol/archive"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html5lib")
+sources = soup.find_all('a', {'class':'quotelink'}, href=True)
+for source in sources: 
+    print("https://boards.4chan.org" + source['href'])
+    writer.writerow(["https://boards.4chan.org" + source['href']])
+file.close()
+
+with open('pol-index.csv','r') as source:
+    lines_src = source.readlines()
+with open('pol-scraped.csv','r') as f:
+    lines_f = f.readlines()
+to_scrape = open("pol-queue.csv","w")
+for data in lines_src:
+    if data not in lines_f:
+        print(data)
+        to_scrape.write(data)
+to_scrape.close()
+exec(open("/home/daniel/shared/pol/pol.py").read())
---
+++ pol/pol.py
@@ -0,0 +1,62 @@
+import collections
+import csv
+import datetime
+import os
+import requests
+import time
+from bs4 import BeautifulSoup
+from requests.exceptions import MissingSchema
+
+my_file = open("pol-queue.csv", "r")
+data = my_file.read()
+data_into_list = data.split("\n")
+urls = collections.deque(data_into_list)
+my_file.close()
+start_time = str(datetime.datetime.now().strftime("%H:%M"))
+while True:
+    dt = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%f"))
+    url = urls[0]
+    try:
+        page = requests.get(url)
+    except MissingSchema or TypeError:
+        print(start_time)
+        end_time = str(datetime.datetime.now().strftime("%H:%M"))
+        print(end_time)
+        break
+    soup = BeautifulSoup(page.content, "html5lib")
+    file = open("4chan-test.txt", "w")
+    scraped = open("pol-scraped.csv", "a")
+    subject = soup.find(class_="subject")
+    posts = soup.find_all(class_="postMessage")
+    idnumbers = soup.find_all("span", class_="hand")
+    users = soup.find_all("span", class_="name")
+    times = soup.find_all("span", class_="dateTime")
+    replied = soup.find_all("a", class_="quotelink")
+    sources = soup.find_all('a', {'title':'Link to this post'}, href=True)
+    while True:
+        writer = csv.writer(file, delimiter="\t")
+        posts = soup.find_all(class_="postMessage")
+        for post in posts:
+            posted = post.get_text(separator=" <br> ").strip()
+        subject = soup.find(class_="subject")
+        idnumbers = soup.find_all("span", class_="hand")
+        users = soup.find_all("span", class_="name")
+        times = soup.find_all("span", class_="dateTime")
+        replied = soup.find_all("a", class_="quotelink")
+        sources = soup.find_all('a', {'title':'Link to this post'}, href=True)
+        urlstr = str(urls[0])
+        try:
+            writer.writerow(["</m " + subject.text + " >"])
+        except AttributeError:
+            writer.writerow(["</m NO SUBJECT >"])
+        for posted, idnumber, user, time, replied, source in zip(posts, idnumbers, users, times, replied, sources):
+            print(posted.text + "\t" + "</m " + idnumber.text + " >" + " \t " + "</m " + user.text + " >" + " \t " + "</m " + time.text + " >" +  " \t " + "</m " + source['href'] + " >" + "\t" + "</m " + str(urls[0]) + " >" + "\t" + "</m " + replied.text + " >")
+            writer.writerow([posted.text, "</m ID# " + idnumber.text + " >", "</m USERNAME " + user.text + " >", "</m TIME " + time.text + " >", "</m POST# " + source['href'] + " >", "</m THREAD " + str(urls[0]) + " >", "</m REPLYING TO " + replied.text + " >"])
+        file.close()
+        newname2 = 'pol'+dt+'.txt'
+        os.rename('4chan-test.txt', newname2)
+        break
+    swriter = csv.writer(scraped)
+    swriter.writerow([str(urls[0])])
+    scraped.close()
+    urls.popleft()
