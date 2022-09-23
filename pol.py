debd67af75278e97bac663aeb7ddce2c7cfdd8e6
ddryl001 <96310117+ddryl001@users.noreply.github.com>
2022-09-22 22:09:32 -0400

    Update pol.py

--- pol.py
+++ pol.py
@@ -25,54 +25,35 @@ while True:
         break
     soup = BeautifulSoup(page.content, "html5lib")
     file = open("4chan-test.txt", "w")
-    file2 = open("4chan-test.tsv", "w")
     scraped = open("pol-scraped.csv", "a")
     subject = soup.find(class_="subject")
     posts = soup.find_all(class_="postMessage")
     idnumbers = soup.find_all("span", class_="hand")
     users = soup.find_all("span", class_="name")
     times = soup.find_all("span", class_="dateTime")
+    replied = soup.find_all("a", class_="quotelink")
     sources = soup.find_all('a', {'title':'Link to this post'}, href=True)
     while True:
-        writer = csv.writer(file)
-        subject = soup.find(class_="subject")
-        try:
-            print(subject.text)
-            writer.writerow([subject.text])
-        except AttributeError:
-            print("")
-            writer.writerow([""])
+        writer = csv.writer(file, delimiter="\t")
         posts = soup.find_all(class_="postMessage")
-        for post in posts:
-            posted = post.get_text(separator=" [BREAK] ").strip()
-            print(posted)
-            writer.writerow([posted])
-        file.close()
-        newname1 = 'pol'+dt+'.txt'
-        os.rename('4chan-test.txt', newname1)
-        break
-    while True:
-        writer = csv.writer(file2)
         subject = soup.find(class_="subject")
-        try:
-            print(subject.text)
-            writer.writerow([subject.text, "ID", "USER", "META", str(urls[0])])
-        except AttributeError:
-            print("")
-            writer.writerow(["", "ID", "USER", "META", str(urls[0])])
-        posts = soup.find_all(class_="postMessage")
-        for post in posts:
-            posted = post.get_text(separator="py").strip()
         idnumbers = soup.find_all("span", class_="hand")
         users = soup.find_all("span", class_="name")
         times = soup.find_all("span", class_="dateTime")
+        replied = soup.find_all("a", class_="quotelink")
         sources = soup.find_all('a', {'title':'Link to this post'}, href=True)
-        for posted, idnumber, user, time, source in zip(posts, idnumbers, users, times, sources):
-            print(posted.text + idnumber.text + " \t " + user.text + " \t " + time.text +  " \t " + str(urls[0]) + source['href'])
-            writer.writerow([posted.text, idnumber.text, user.text, time.text, source['href']])
-        file2.close()
-        newname2 = 'pol'+dt+'.tsv'
-        os.rename('4chan-test.tsv', newname2)
+        urlstr = str(urls[0])
+        try:
+            writer.writerow(["<m> " + subject.text + " </m>"])
+        except AttributeError:
+            writer.writerow(["<m> NO SUBJECT </m>"])
+        for post, idnumber, user, time, replied, source in zip(posts, idnumbers, users, times, replied, sources):
+            post = post.get_text(separator=" <break> ")
+            print(post + "\t" + "</m " + idnumber.text + " >" + " \t " + "</m " + user.text + " >" + " \t " + "</m " + time.text + " >" +  " \t " + "</m " + source['href'] + " >" + "\t" + "</m " + str(urls[0]) + " >" + "\t" + "</m " + replied.text + " >")
+            writer.writerow([post, "<m> ID# " + idnumber.text + " </m>", "<m> USERNAME " + user.text + " </m>", "<m> TIME " + time.text + " </m>", "<m> POST# " + source['href'] + " </m>", "<m> THREAD " + str(urls[0]) + " </m>", "<m> REPLYING TO " + replied.text + " </m>"])
+        file.close()
+        newname2 = 'pol'+dt+'.txt'
+        os.rename('4chan-test.txt', newname2)
         break
     swriter = csv.writer(scraped)
     swriter.writerow([str(urls[0])])
