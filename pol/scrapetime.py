e85cadc8b9c5b5774e80b6c79f808ab1520f79c7
ddryl001 <96310117+ddryl001@users.noreply.github.com>
2022-09-23 00:35:50 -0400

    Update scrapetime.py

--- pol/scrapetime.py
+++ pol/scrapetime.py
@@ -6,17 +6,11 @@ def pol():
     dt = str(datetime.datetime.now().strftime("%H:%M"))
     print("/pol/ done " + dt)
 
-def qresearch():
-    exec(open("/home/daniel/shared/qresearch/qresearch-index.py").read())
-    dt = str(datetime.datetime.now().strftime("%H:%M"))
-    print("/qresearch/ done " + dt)
-
 import time
 import schedule
 from schedule import every, run_pending
 
 schedule.every(5).minutes.do(pol)
-#schedule.every(12).hours.do(qresearch)
 
 while True: 
     run_pending()
